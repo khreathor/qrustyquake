904fa00a85ab34a3e42513c4bfaf4703059a851d
Cyan <cyanbun96@gmail.com>
2025-01-03 20:21:22 +0200

    Unreplaced library replacement functions

--- src/common.c
+++ src/common.c
@@ -142,300 +142,102 @@ void InsertLinkAfter (link_t *l, link_t *after)
 	l->next->prev = l;
 }
 
-/*
-============================================================================
+// =============================================================================
+// Library Replacement Functions
+// =============================================================================
 
-					LIBRARY REPLACEMENT FUNCTIONS
-
-============================================================================
-*/
+// CyanBun96: the standard functions should be faster and more reliable, and
+// the benefits of replacing them are not there three decades later.
+// The prototypes are kept the same for compatibility, some non-standard
+// behavior is preserved (like the return values of memcmp and strcmp)
 
 void Q_memset (void *dest, int fill, size_t count)
 {
-	size_t		i;
-
-	if ( (((size_t)dest | count) & 3) == 0)
-	{
-		count >>= 2;
-		fill = fill | (fill<<8) | (fill<<16) | (fill<<24);
-		for (i=0 ; i<count ; i++)
-			((int *)dest)[i] = fill;
-	}
-	else
-		for (i=0 ; i<count ; i++)
-			((byte *)dest)[i] = fill;
+	memset (dest, fill, count);
 }
 
 void Q_memcpy (void *dest, const void *src, size_t count)
 {
-	size_t		i;
-
-	if (( ( (size_t)dest | (size_t)src | count) & 3) == 0 )
-	{
-		count>>=2;
-		for (i=0 ; i<count ; i++)
-			((int *)dest)[i] = ((int *)src)[i];
-	}
-	else
-		for (i=0 ; i<count ; i++)
-			((byte *)dest)[i] = ((byte *)src)[i];
+	memcpy (dest, src, count);
 }
 
 int Q_memcmp (const void *m1, const void *m2, size_t count)
 {
-	while(count)
-	{
-		count--;
-		if (((byte *)m1)[count] != ((byte *)m2)[count])
-			return -1;
-	}
-	return 0;
+	return memcmp (m1, m2, count) == 0 ? 0 : -1;
 }
 
 void Q_strcpy (char *dest, const char *src)
 {
-	while (*src)
-	{
-		*dest++ = *src++;
-	}
-	*dest++ = 0;
+	strcpy (dest, src);
 }
 
 void Q_strncpy (char *dest, const char *src, int count)
 {
-	while (*src && count--)
-	{
-		*dest++ = *src++;
-	}
-	if (count)
-		*dest++ = 0;
+	strncpy (dest, src, count);
 }
 
 int Q_strlen (const char *str)
 {
-	int             count;
-
-	count = 0;
-	while (str[count])
-		count++;
-
-	return count;
+	return strlen (str);
 }
 
-char *Q_strrchr(const char *s, char c)
+char *Q_strrchr (const char *s, char c)
 {
-    int len = Q_strlen(s);
-    s += len;
-    while (len--)
-	if (*--s == c) return (char *)s;
-    return NULL;
+	return strrchr (s, c);
 }
 
 void Q_strcat (char *dest, const char *src)
 {
-	dest += Q_strlen(dest);
-	Q_strcpy (dest, src);
+	strcat (dest, src);
 }
 
 int Q_strcmp (const char *s1, const char *s2)
 {
-	while (1)
-	{
-		if (*s1 != *s2)
-			return -1;              // strings not equal
-		if (!*s1)
-			return 0;               // strings are equal
-		s1++;
-		s2++;
-	}
-
-	return -1;
+	return strcmp (s1, s2) == 0 ? 0 : -1;
 }
 
 int Q_strncmp (const char *s1, const char *s2, int count)
 {
-	while (1)
-	{
-		if (!count--)
-			return 0;
-		if (*s1 != *s2)
-			return -1;              // strings not equal
-		if (!*s1)
-			return 0;               // strings are equal
-		s1++;
-		s2++;
-	}
-
-	return -1;
+	return strncmp (s1, s2, count) == 0 ? 0 : -1;
 }
 
 int Q_strncasecmp (const char *s1, const char *s2, int n)
 {
-	int             c1, c2;
-
-	while (1)
-	{
-		c1 = *s1++;
-		c2 = *s2++;
-
-		if (!n--)
-			return 0;               // strings are equal until end point
-
-		if (c1 != c2)
-		{
-			if (c1 >= 'a' && c1 <= 'z')
-				c1 -= ('a' - 'A');
-			if (c2 >= 'a' && c2 <= 'z')
-				c2 -= ('a' - 'A');
-			if (c1 != c2)
-				return -1;              // strings not equal
-		}
-		if (!c1)
-			return 0;               // strings are equal
-//              s1++;
-//              s2++;
-	}
-
-	return -1;
+	return strncasecmp (s1, s2, n) == 0 ? 0 : -1;
 }
 
 int Q_strcasecmp (const char *s1, const char *s2)
 {
-	return Q_strncasecmp (s1, s2, 99999);
+	return strcasecmp (s1, s2) == 0 ? 0 : -1;
 }
 
-int Q_atoi (const char *str)
+int Q_atoi(const char *str)
 {
-	int             val;
-	int             sign;
-	int             c;
-
-	if (*str == '-')
-	{
-		sign = -1;
-		str++;
-	}
-	else
-		sign = 1;
-
-	val = 0;
-
-//
-// check for hex
-//
-	if (str[0] == '0' && (str[1] == 'x' || str[1] == 'X') )
-	{
-		str += 2;
-		while (1)
-		{
-			c = *str++;
-			if (c >= '0' && c <= '9')
-				val = (val<<4) + c - '0';
-			else if (c >= 'a' && c <= 'f')
-				val = (val<<4) + c - 'a' + 10;
-			else if (c >= 'A' && c <= 'F')
-				val = (val<<4) + c - 'A' + 10;
-			else
-				return val*sign;
-		}
-	}
-
-//
-// check for character
-//
-	if (str[0] == '\'')
-	{
-		return sign * str[1];
-	}
-
-//
-// assume decimal
-//
-	while (1)
-	{
-		c = *str++;
-		if (c <'0' || c > '9')
-			return val*sign;
-		val = val*10 + c - '0';
-	}
-
-	return 0;
+    int sign = 1;
+    if (*str == '-') {
+        sign = -1;
+        str++;
+    }
+    if (str[0] == '0' && (str[1] == 'x' || str[1] == 'X')) // Check for hex
+        return sign * (int)strtol(str, NULL, 16);
+    // Check for character constant
+    if (str[0] == '\'' && str[1] != '\0') // Check for character constant
+        return sign * (int)str[1];
+    return sign * atoi(str); // Assume decimal
 }
 
-
-float Q_atof (const char *str)
+float Q_atof(const char *str)
 {
-	double			val;
-	int             sign;
-	int             c;
-	int             decimal, total;
-
-	if (*str == '-')
-	{
-		sign = -1;
-		str++;
-	}
-	else
-		sign = 1;
-
-	val = 0;
-
-//
-// check for hex
-//
-	if (str[0] == '0' && (str[1] == 'x' || str[1] == 'X') )
-	{
-		str += 2;
-		while (1)
-		{
-			c = *str++;
-			if (c >= '0' && c <= '9')
-				val = (val*16) + c - '0';
-			else if (c >= 'a' && c <= 'f')
-				val = (val*16) + c - 'a' + 10;
-			else if (c >= 'A' && c <= 'F')
-				val = (val*16) + c - 'A' + 10;
-			else
-				return val*sign;
-		}
-	}
-
-//
-// check for character
-//
-	if (str[0] == '\'')
-	{
-		return sign * str[1];
-	}
-
-//
-// assume decimal
-//
-	decimal = -1;
-	total = 0;
-	while (1)
-	{
-		c = *str++;
-		if (c == '.')
-		{
-			decimal = total;
-			continue;
-		}
-		if (c <'0' || c > '9')
-			break;
-		val = val*10 + c - '0';
-		total++;
-	}
-
-	if (decimal == -1)
-		return val*sign;
-	while (total > decimal)
-	{
-		val /= 10;
-		total--;
-	}
-
-	return val*sign;
+    int sign = 1;
+    if (*str == '-') {
+        sign = -1;
+        str++;
+    }
+    if (str[0] == '0' && (str[1] == 'x' || str[1] == 'X')) // Check for hex
+        return (float)(sign * strtod(str, NULL));
+    if (str[0] == '\'' && str[1] != '\0') // Check for character constant
+        return (float)(sign * (int)str[1]);
+    return (float)(sign * strtod(str, NULL)); // Assume decimal
 }
 
 /*
