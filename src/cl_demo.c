a4c8fd779e0710cda9344447d443a00ec0691c25
Cyan <cyanbun96@gmail.com>
2025-01-09 23:04:31 +0200

    cl_demo.c refactoring

--- src/cl_demo.c
+++ src/cl_demo.c
@@ -1,98 +1,52 @@
-/*
-Copyright (C) 1996-2001 Id Software, Inc.
-Copyright (C) 2002-2005 John Fitzgibbons and others
-Copyright (C) 2007-2008 Kristian Duske
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-
-*/
+// Copyright (C) 1996-2001 Id Software, Inc.
+// Copyright (C) 2002-2005 John Fitzgibbons and others
+// Copyright (C) 2007-2008 Kristian Duske
+// GPLv3 See LICENSE for details.
 
 #include "quakedef.h"
 
-void CL_FinishTimeDemo(void);
+// When a demo is playing back, all NET_SendMessages are skipped, and
+// NET_GetMessages are read from the demo file.
+// Whenever cl.time gets past the last received message, another message is
+// read from the demo file.
 
-/*
-==============================================================================
-
-DEMO CODE
-
-When a demo is playing back, all NET_SendMessages are skipped, and
-NET_GetMessages are read from the demo file.
-
-Whenever cl.time gets past the last received message, another message is
-read from the demo file.
-==============================================================================
-*/
-
-/*
-==============
-CL_StopPlayback
-
-Called when a demo file runs out, or the user starts a game
-==============
-*/
-void CL_StopPlayback(void)
+void CL_FinishTimeDemo()
 {
+	cls.timedemo = false; // the first frame didn't count
+	int frames = (host_framecount - cls.td_startframe) - 1;
+	float time = realtime - cls.td_starttime;
+	if (!time)
+		time = 1;
+	Con_Printf("%i frames %5.1f seconds %5.1f fps\n", frames, time,
+		   frames / time);
+}
+
+void CL_StopPlayback()
+{ // Called when a demo file runs out, or the user starts a game
 	if (!cls.demoplayback)
 		return;
-
 	fclose(cls.demofile);
 	cls.demoplayback = false;
 	cls.demofile = NULL;
 	cls.state = ca_disconnected;
-
 	if (cls.timedemo)
 		CL_FinishTimeDemo();
 }
 
-/*
-====================
-CL_WriteDemoMessage
-
-Dumps the current net message, prefixed by the length and view angles
-====================
-*/
-void CL_WriteDemoMessage(void)
-{
-	int len;
-	int i;
-	float f;
-
-	len = LittleLong(net_message.cursize);
+void CL_WriteDemoMessage()
+{ // Dumps the current net message, prefixed by the length and view angles
+	int len = LittleLong(net_message.cursize);
 	fwrite(&len, 4, 1, cls.demofile);
-	for (i = 0; i < 3; i++) {
-		f = LittleFloat(cl.viewangles[i]);
+	for (int i = 0; i < 3; i++) {
+		float f = LittleFloat(cl.viewangles[i]);
 		fwrite(&f, 4, 1, cls.demofile);
 	}
 	fwrite(net_message.data, net_message.cursize, 1, cls.demofile);
 	fflush(cls.demofile);
 }
 
-/*
-====================
-CL_GetMessage
-
-Handles recording and playback of demos, on top of NET_ code
-====================
-*/
-int CL_GetMessage(void)
-{
-	int r, i;
-	float f;
-
+int CL_GetMessage()
+{ // Handles recording and playback of demos, on top of NET_ code
 	if (cls.demoplayback) {
 		// decide if it is time to grab the next message
 		if (cls.signon == SIGNONS)	// allways grab until fully connected
@@ -112,240 +66,140 @@ int CL_GetMessage(void)
 		// get the next message
 		fread(&net_message.cursize, 4, 1, cls.demofile);
 		VectorCopy(cl.mviewangles[0], cl.mviewangles[1]);
-		for (i = 0; i < 3; i++) {
-			r = fread(&f, 4, 1, cls.demofile);
+		float f;
+		for (int i = 0; i < 3; i++) {
+			fread(&f, 4, 1, cls.demofile);
 			cl.mviewangles[0][i] = LittleFloat(f);
 		}
-
 		net_message.cursize = LittleLong(net_message.cursize);
 		if (net_message.cursize > MAX_MSGLEN)
 			Sys_Error("Demo message > MAX_MSGLEN");
-		r = fread(net_message.data, net_message.cursize, 1,
-			  cls.demofile);
-		if (r != 1) {
+		if (fread(net_message.data, net_message.cursize,
+				1, cls.demofile) != 1) {
 			CL_StopPlayback();
 			return 0;
 		}
-
 		return 1;
 	}
-
+	int r;
 	while (1) {
 		r = NET_GetMessage(cls.netcon);
-
 		if (r != 1 && r != 2)
 			return r;
-
 		// discard nop keepalive message
 		if (net_message.cursize == 1 && net_message.data[0] == svc_nop)
 			Con_Printf("<-- server to client keepalive\n");
 		else
 			break;
 	}
-
 	if (cls.demorecording)
 		CL_WriteDemoMessage();
-
 	return r;
 }
 
-/*
-====================
-CL_Stop_f
-
-stop recording a demo
-====================
-*/
-void CL_Stop_f(void)
-{
+void CL_Stop_f()
+{ // stop recording a demo
 	if (cmd_source != src_command)
 		return;
-
 	if (!cls.demorecording) {
 		Con_Printf("Not recording a demo.\n");
 		return;
 	}
-// write a disconnect message to the demo file
-	SZ_Clear(&net_message);
+	SZ_Clear(&net_message); // write a disconnect message to the demo file
 	MSG_WriteByte(&net_message, svc_disconnect);
 	CL_WriteDemoMessage();
-
-// finish up
-	fclose(cls.demofile);
+	fclose(cls.demofile); // finish up
 	cls.demofile = NULL;
 	cls.demorecording = false;
 	Con_Printf("Completed demo\n");
 }
 
-/*
-====================
-CL_Record_f
 
-record <demoname> <map> [cd track]
-====================
-*/
-void CL_Record_f(void)
-{
-	int c;
-	char name[MAX_OSPATH];
-	int track;
-
+void CL_Record_f()
+{ // record <demoname> <map> [cd track]
 	if (cmd_source != src_command)
 		return;
-
-	c = Cmd_Argc();
+	int c = Cmd_Argc();
 	if (c != 2 && c != 3 && c != 4) {
 		Con_Printf("record <demoname> [<map> [cd track]]\n");
 		return;
 	}
-
 	if (strstr(Cmd_Argv(1), "..")) {
 		Con_Printf("Relative pathnames are not allowed.\n");
 		return;
 	}
-
 	if (c == 2 && cls.state == ca_connected) {
 		Con_Printf
 		    ("Can not record - already connected to server\nClient demo recording must be started before connecting\n");
 		return;
 	}
-// write the forced cd track number, or -1
+	int track = -1; // write the forced cd track number, or -1
 	if (c == 4) {
 		track = atoi(Cmd_Argv(3));
 		Con_Printf("Forcing CD track to %i\n", cls.forcetrack);
-	} else
-		track = -1;
-
+	}
+	char name[MAX_OSPATH+2];
 	sprintf(name, "%s/%s", com_gamedir, Cmd_Argv(1));
-
-//
-// start the map up
-//
-	if (c > 2)
+	if (c > 2) // start the map up
 		Cmd_ExecuteString(va("map %s", Cmd_Argv(2)), src_command);
-
-//
-// open the demo file
-//
-	COM_DefaultExtension(name, ".dem");
-
+	COM_DefaultExtension(name, ".dem"); // open the demo file
 	Con_Printf("recording to %s.\n", name);
 	cls.demofile = fopen(name, "wb");
 	if (!cls.demofile) {
 		Con_Printf("ERROR: couldn't open.\n");
 		return;
 	}
-
 	cls.forcetrack = track;
 	fprintf(cls.demofile, "%i\n", cls.forcetrack);
-
 	cls.demorecording = true;
 }
 
-/*
-====================
-CL_PlayDemo_f
 
-play [demoname]
-====================
-*/
-void CL_PlayDemo_f(void)
-{
-	char name[256];
-	int c;
-	qboolean neg = false;
-
+void CL_PlayDemo_f()
+{ // play [demoname]
 	if (cmd_source != src_command)
 		return;
-
 	if (Cmd_Argc() != 2) {
 		Con_Printf("play <demoname> : plays a demo\n");
 		return;
 	}
-	// withdraw console/menu
-	if (key_dest == key_console)
+	if (key_dest == key_console) // withdraw console/menu
 		key_dest = key_game;
-
-//
-// disconnect from server
-//
-	CL_Disconnect();
-
-//
-// open the demo file
-//
-	strcpy(name, Cmd_Argv(1));
+	CL_Disconnect(); // disconnect from server
+	char name[256];
+	strcpy(name, Cmd_Argv(1)); // open the demo file
 	COM_DefaultExtension(name, ".dem");
-
 	Con_Printf("Playing demo from %s.\n", name);
 	COM_FOpenFile(name, &cls.demofile);
 	if (!cls.demofile) {
 		Con_Printf("ERROR: couldn't open.\n");
-		cls.demonum = -1;	// stop demo loop
+		cls.demonum = -1; // stop demo loop
 		return;
 	}
-
 	cls.demoplayback = true;
 	cls.state = ca_connected;
 	cls.forcetrack = 0;
-
+	int c;
+	qboolean neg = false;
 	while ((c = getc(cls.demofile)) != '\n')
 		if (c == '-')
 			neg = true;
 		else
 			cls.forcetrack = cls.forcetrack * 10 + (c - '0');
-
 	if (neg)
 		cls.forcetrack = -cls.forcetrack;
-// ZOID, fscanf is evil
-//      fscanf (cls.demofile, "%i\n", &cls.forcetrack);
 }
 
-/*
-====================
-CL_FinishTimeDemo
-
-====================
-*/
-void CL_FinishTimeDemo(void)
-{
-	int frames;
-	float time;
-
-	cls.timedemo = false;
-
-// the first frame didn't count
-	frames = (host_framecount - cls.td_startframe) - 1;
-	time = realtime - cls.td_starttime;
-	if (!time)
-		time = 1;
-	Con_Printf("%i frames %5.1f seconds %5.1f fps\n", frames, time,
-		   frames / time);
-}
-
-/*
-====================
-CL_TimeDemo_f
-
-timedemo [demoname]
-====================
-*/
-void CL_TimeDemo_f(void)
-{
+void CL_TimeDemo_f()
+{ // timedemo [demoname]
 	if (cmd_source != src_command)
 		return;
-
 	if (Cmd_Argc() != 2) {
 		Con_Printf("timedemo <demoname> : gets demo speeds\n");
 		return;
 	}
-
 	CL_PlayDemo_f();
-
-// cls.td_starttime will be grabbed at the second frame of the demo, so
-// all the loading time doesn't get counted
-
-	cls.timedemo = true;
-	cls.td_startframe = host_framecount;
+	cls.timedemo = true; // cls.td_starttime will be grabbed at the second frame of the demo,
+	cls.td_startframe = host_framecount; // so all the loading time doesn't get counted
 	cls.td_lastframe = -1;	// get a new message this frame
 }
--- src/progdefs.h
+++
@@ -1,7 +0,0 @@
-// Copyright (C) 1996-1997 Id Software, Inc. GPLv3 See LICENSE for details.
-
-#ifdef QUAKE2
-#include "progdefs.q2"
-#else
-#include "progdefs.q1"
-#endif
