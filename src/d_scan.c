d449098052d57a8130dc61ab1a82a053ab060255
Cyan <cyanbun96@gmail.com>
2025-06-10 15:41:10 +0300

    Fixed transparent brush rendering

--- src/d_scan.c
+++ src/d_scan.c
@@ -275,6 +275,7 @@ void D_DrawSpans(espan_t *pspan, s32 type, f32 opacity)
 	f32 sdivz8stepu = d_sdivzstepu * 8;
 	f32 tdivz8stepu = d_tdivzstepu * 8;
 	f32 zi8stepu = d_zistepu * 8;
+	izistep = (s32)(d_zistepu * 0x8000 * 0x10000);
 	do {
 		u8 *pdest = (u8 *)((u8 *) d_viewbuffer +
 				      (screenwidth * pspan->v) + pspan->u);
@@ -293,7 +294,7 @@ void D_DrawSpans(espan_t *pspan, s32 type, f32 opacity)
 		f32 tdivz = d_tdivzorigin + dv*d_tdivzstepv + du*d_tdivzstepu;
 		f32 zi = d_ziorigin + dv * d_zistepv + du * d_zistepu;
 		f32 z = (f32)0x10000 / zi; // prescale to 16.16 fixed-point
-		if (type == SPAN_TRANS) { // FIXME? appears kinda broken
+		if (type == SPAN_TRANS) {
 			pz = d_pzbuffer + (d_zwidth * pspan->v) + pspan->u;
 			izi = (s32)(zi * 0x8000 * 0x10000);
 		}
