5749f9eb4e0a8e49dd8a8e4ee79123425b0e8b9b
erysdren <contact@erysdren.me>
2025-07-10 21:52:17 -0500

    cdaudio: iterate all available formats in SDL_Mixer

--- src/cdaudio.c
+++ src/cdaudio.c
@@ -7,6 +7,29 @@
 // FIXME: add to globals.h
 u8 *COM_LoadTempFile(const s8 *path, u32 *path_id);
 
+#ifndef ASIZE
+#define ASIZE(a) (sizeof(a)/sizeof(a[0]))
+#endif
+
+// erysdren: note that i've placed these in a particular order, so that when
+// they're iterated in CDAudio_Play() the most common formats for Quake mod
+// music will come up first
+static struct music_format {
+	u32 flag;
+	const char *name;
+	size_t num_extensions;
+	const char **extensions;
+	bool required;
+} music_formats[] = {
+	{MIX_INIT_OGG, "OGG", 1, (const char *[]){".ogg"}, true},
+	{MIX_INIT_OPUS, "OPUS", 2, (const char *[]){".ogg", ".opus"}, true},
+	{MIX_INIT_MP3, "MP3", 1, (const char *[]){".mp3"}, true},
+	{MIX_INIT_FLAC, "FLAC", 1, (const char *[]){".flac"}, false},
+	{MIX_INIT_MID, "MID", 2, (const char *[]){".mid", ".midi"}, false},
+	{MIX_INIT_MOD, "MOD", 1, (const char *[]){".mod"}, false},
+	{MIX_INIT_WAVPACK, "WAVPACK", 2, (const char *[]){".wav", ".wv"}, false}
+};
+
 static Mix_Music *current_music = NULL;
 
 void CDAudio_Play(u8 track, bool looping)
@@ -15,20 +38,26 @@ void CDAudio_Play(u8 track, bool looping)
 	u8 *file = NULL;
 	SDL_IOStream *io = NULL;
 	Mix_Music *music = NULL;
+	size_t i, j;
 
-	q_snprintf(filename, sizeof(filename), "music/track%02d.ogg", (int)track);
-
-	file = COM_LoadTempFile(filename, NULL);
-	if (!file)
+	for (i = 0; i < ASIZE(music_formats); i++)
 	{
-		q_snprintf(filename, sizeof(filename), "music/track%02d.mp3", (int)track);
-		file = COM_LoadTempFile(filename, NULL);
-		if (!file)
+		for (j = 0; j < music_formats[i].num_extensions; j++)
 		{
-			Con_Printf("file for track %02d not found\n", (int)track);
-			return;
+			q_snprintf(filename, sizeof(filename), "music/track%02d%s", (int)track, music_formats[i].extensions[j]);
+			file = COM_LoadTempFile(filename, NULL);
+			if (file)
+				goto found;
 		}
 	}
+
+	if (!file)
+	{
+		Con_Printf("file for track %02d not found\n", (int)track);
+		return;
+	}
+
+found:
 
 	io = SDL_IOFromConstMem(file, com_filesize);
 	if (!io)
@@ -87,17 +116,22 @@ void CDAudio_Update()
 
 bool CDAudio_Init()
 {
-	// FIXME: more formats? other ones that mod care about?
-	MIX_InitFlags initflags = Mix_Init(MIX_INIT_MP3 | MIX_INIT_OGG | MIX_INIT_OPUS);
-	if (!(initflags & MIX_INIT_MP3) || !(initflags & MIX_INIT_OGG) || !(initflags & MIX_INIT_OPUS))
+	size_t i;
+	for (i = 0; i < ASIZE(music_formats); i++)
 	{
-		Con_Printf("Failed to initialize SDL Mixer: %s\n", SDL_GetError());
-		return false;
+		MIX_InitFlags flags = Mix_Init(music_formats[i].flag);
+		if (!(flags & music_formats[i].flag))
+		{
+			Con_Printf("Failed to initialize SDL Mixer format \"%s\": %s\n", music_formats[i].name, SDL_GetError());
+			if (music_formats[i].required)
+				return false;
+		}
+		Con_Printf("Initialized SDL Mixer format \"%s\"\n", music_formats[i].name);
 	}
 
 	if (!Mix_OpenAudio(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, NULL))
 	{
-		Con_Printf("Failed to initialize SDL Mixer: %s\n", SDL_GetError());
+		Con_Printf("Failed to SDL Mixer audio output device: %s\n", SDL_GetError());
 		return false;
 	}
 
